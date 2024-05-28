d63231043f7b6d7ebcc6961b59afdb4dc1640929
matthew-akinloye <akinloyematthew89@gmail.com>
2024-05-28 11:29:00 +0100

    po

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
     SECRET_KEY = 'ewrfsder43344rf'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = "postgresql://default:sMjv1FmZ6Dnr@ep-falling-mouse-40250356.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
