6354491f74bf670b902865956c59672b8f07c129
matthew-akinloye <akinloyematthew89@gmail.com>
2024-05-28 11:19:45 +0100

    change

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
     SECRET_KEY = 'ewrfsder43344rf'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://default:XHEYSl5Cax7e@ep-aged-meadow-a42wsudx-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
