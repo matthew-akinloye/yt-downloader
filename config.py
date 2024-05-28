4f5d8abc025e81389020d48337b3f4e742ba9020
matthew-akinloye <akinloyematthew89@gmail.com>
2024-05-28 11:58:24 +0100

    c

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# config.py
+
 class Config:
     SECRET_KEY = 'ewrfsder43344rf'
     SQLALCHEMY_DATABASE_URI = "postgresql://default:XHEYSl5Cax7e@ep-aged-meadow-a42wsudx-pooler.us-east-1.aws.neon.tech:5432/verceldb"
