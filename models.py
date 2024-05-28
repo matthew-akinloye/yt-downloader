450d0eb062b2c6111cd52d89d55f02dde66f6077
matthew-akinloye <akinloyematthew89@gmail.com>
2024-05-28 12:02:22 +0100

    pi

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
@@ -19,6 +20,7 @@ class User(db.Model):
         
         
 class History(db.Model):
+    __tablename__ = 'history'
     id = db.Column(db.Integer, primary_key=True)
     video_title = db.Column(db.String(200), nullable=False)
     video_url = db.Column(db.String(200), nullable=False)
